fa517dd5692a0e2ef0a8fad14a6ee7e2a7bb4a61
Helo Gigliott <heloisa.soroca@edu.senai.br>
2026-07-31 15:14:29 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/Atividade 01/estante.js
@@ -0,0 +1,16 @@
+// ===== LIVRO 1 =====
+const livro1 = {
+titulo: "Clean Code",
+autor: "Robert C. Martin",
+preco: 89.9,
+estoque: 12,
+};
+// ===== LIVRO 2 — TROQUE os quatro valores abaixo =====
+const livro2 = {
+titulo: "Amor, teoricamente",
+autor: "Ali Hazelwood",
+preco: 65.5,
+estoque: 10,
+};
+// Esta linha entrega os dois livros para os outros arquivos usarem
+module.exports = { livro1, livro2 };
